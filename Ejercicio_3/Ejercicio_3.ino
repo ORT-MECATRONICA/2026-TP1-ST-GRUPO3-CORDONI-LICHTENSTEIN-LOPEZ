231664b8bea63d779e8a241586b244c3327eeda4
tinocordoni <cordoniderosa@gmail.com>
2026-03-31 15:41:48 -0300

    Ejercicio 3

--- Ejercicio_3/Ejercicio_3.ino
+++ Ejercicio_3/Ejercicio_3.ino
@@ -5,7 +5,7 @@ Lichtenstein
 López
 */
 
-//Ejercicio 2
+//Ejercicio 3
 #include <DHT.h>
 #include <U8g2lib.h>
 
@@ -15,12 +15,15 @@ López
 DHT dht(DATA_DHT, DHTTYPE);
 U8G2_SH1106_128X64_NONAME_F_HW_I2C u8g2(U8G2_R0, U8X8_PIN_NONE);
 
-
+#define UMBRAL 28
+#define LED_1 25
 
 void setup() {
   Serial.begin(9600);
   dht.begin();
   u8g2.begin();
+
+  pinMode(LED_1, OUTPUT);
 }
 
 void loop() {
@@ -32,6 +35,12 @@ void loop() {
   sprintf(stringtemp, "%.1f C", temperatura);
   u8g2.drawStr(30, 50, stringtemp);
   u8g2.sendBuffer();
+
+  if (temperatura >= UMBRAL){
+    Serial.digitalWrite(LED_1, HIGH);
+  } else {
+    Serial.digitalWrite(LED_1, LOW);
+  }
   
   delay(2000);  // 2 segundos mínimo
 }
