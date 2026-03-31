109ae95a39f5a59b39cd4a99f47af17b291b737c
tinocordoni <cordoniderosa@gmail.com>
2026-03-31 15:19:43 -0300

    errores

--- Ejercicio_2/Ejercicio_2.ino
+++ Ejercicio_2/Ejercicio_2.ino
@@ -9,25 +9,19 @@ López
 #include <DHT.h>
 #include <U8g2lib.h>
 
-#define DATA_DHT 23    // pin del dht
-#define DHTTYPE DHT11  // tipo de DHT
-
-DHT dht(DATA_DHT, DHTTYPE);
-U8G2_SH1106_128X64_NONAME_F_HW_I2C u8g2(U8G2_R0, /* reset=*/ U8X8_PIN_NONE);
-
-void printBMP_OLED(void );
+U8G2_SH1106_128X64_NONAME_F_HW_I2C u8g2(U8G2_R0, /* reset=*/U8X8_PIN_NONE);
 
 void setup() {
   Serial.begin(9600);
-  dht.begin();
+  u8g2.begin();
 }
 
 void loop() {
-  float temperatura = dht.readTemperature();
 
-  Serial.print("Temperatura: ");
-  Serial.print(temperatura);
-  Serial.println(" °C");
+  u8g2.clearBuffer();
+  u8g2.setFont(u8g2_font_ncenB14_tr);
+  u8g2.drawStr(10, 30, "Hola, Mirko");
+  u8g2.sendBuffer();
 
   delay(2000);  // 2 segundos mínimo
 }
---
+++ Ejercicio_3/EJercicio_3/EJercicio_3.ino
@@ -0,0 +1,33 @@
+/*
+Grupo 3
+Cordoni
+Lichtenstein
+López
+*/
+
+//Ejercicio 3
+#include <DHT.h>
+#include <U8g2lib.h>
+
+#define DATA_DHT 23    // pin del dht
+#define DHTTYPE DHT11  // tipo de DHT
+
+DHT dht(DATA_DHT, DHTTYPE);
+U8G2_SH1106_128X64_NONAME_F_HW_I2C u8g2(U8G2_R0, /* reset=*/ U8X8_PIN_NONE);
+
+void printBMP_OLED(void );
+
+void setup() {
+  Serial.begin(9600);
+  dht.begin();
+}
+
+void loop() {
+  float temperatura = dht.readTemperature();
+
+  Serial.print("Temperatura: ");
+  Serial.print(temperatura);
+  Serial.println(" °C");
+
+  delay(2000);  // 2 segundos mínimo
+}
