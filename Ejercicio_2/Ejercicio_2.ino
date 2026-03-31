17f645c2b5e6f1bfd87c93dff61322a71000a7d7
tinocordoni <cordoniderosa@gmail.com>
2026-03-31 14:27:39 -0300

    Update Ejercicio_2.ino

--- Ejercicio_2/Ejercicio_2.ino
+++ Ejercicio_2/Ejercicio_2.ino
@@ -7,11 +7,15 @@ López
 
 //Ejercicio 2
 #include <DHT.h>
+#include <U8g2lib.h>
 
 #define DATA_DHT 23    // pin del dht
 #define DHTTYPE DHT11  // tipo de DHT
 
 DHT dht(DATA_DHT, DHTTYPE);
+U8G2_SH1106_128X64_NONAME_F_HW_I2C u8g2(U8G2_R0, /* reset=*/ U8X8_PIN_NONE);
+
+void printBMP_OLED(void );
 
 void setup() {
   Serial.begin(9600);
