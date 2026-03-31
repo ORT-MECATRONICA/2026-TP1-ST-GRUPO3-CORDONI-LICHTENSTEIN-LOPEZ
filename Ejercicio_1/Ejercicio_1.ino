d2b7dce2f36267190ba1e1fd8e222b6aa8cce7b4
tinocordoni <cordoniderosa@gmail.com>
2026-03-31 14:40:37 -0300

    Update Ejercicio_1.ino

--- Ejercicio_1/Ejercicio_1.ino
+++ Ejercicio_1/Ejercicio_1.ino
@@ -7,10 +7,19 @@ López
 
 //Ejercicio 1
 
+#include <U8g2lib.h>
+
+U8G2_SH1106_128X64_NONAME_F_HW_I2C u8g2(U8G2_R0, U8X8_PIN_NONE);
+
 void setup() {
   Serial.begin(9600);
+  u8g2.begin();
 }
 
 void loop() {
-  Serial.println("Hola, Mirko");
+  u8g2.clearBuffer();
+  u8g2.setFont(u8g2_font_ncenB14_tr);
+  u8g2.drawStr(10, 30, "Hola, Mirko");
+  u8g2.sendBuffer();
+  delay(2000);
 }
